709b4b468f323d85d34046d1b39d45f0f9512371
nkhachan <noopur.khachane@gmail.com>
2018-01-13 16:25:02 -0800

    added scripts for two arduinos

---
+++ Arduino Scripts/Arduino_in_Press_Table.ino
@@ -0,0 +1,80 @@
+/*
+Local Arduino in Bunker
+ */
+struct info_t
+{
+  byte length;
+  byte id;
+  byte driver1;
+  byte driver2;
+  byte driver3;
+  byte driver4;
+  byte driver5;
+  byte driver6;
+  byte pot_reading;
+  byte automatic;
+};
+
+// Global variable to hold State of Health tlm packet.
+info_t information;
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+  information.id = 1;
+  
+  for(int i=0; i<6; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+  
+  Serial.begin(57600);  // initialize serial port to transceiver and COSMOS
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  processCmds();
+  setDrivers();
+  setServo();
+}
+
+void processCmds()
+{
+  while (Serial.available())
+  {
+    // get the new byte:
+    information.driver1 = Serial.read();
+    information.driver2 = Serial.read();
+    information.driver3 = Serial.read();
+    information.driver4 = Serial.read();
+    information.driver5 = Serial.read();
+    information.driver6 = Serial.read();
+    information.pot_reading = Serial.read();
+    information.automatic = Serial.read();
+  }
+}
+
+void setDrivers()
+{
+  digitalWrite(0, driver.driver1);
+  digitalWrite(1, driver.driver2);
+  digitalWrite(2, driver.driver3);
+  digitalWrite(3, driver.driver4);
+  digitalWrite(4, driver.driver5);
+  digitalWrite(5, driver.driver6);
+}
+
+void setServo()
+{
+  if (information.automatic)
+  {
+     //Servo do some shit
+     //PID function
+  }
+  else
+  {
+    //Servo do some shit
+    //Get shit from pot_reading
+  }
+}
---
+++ Arduino Scripts/Local_Arduino_in_Bunker.ino
@@ -0,0 +1,62 @@
+/*
+Local Arduino in Bunker
+ */
+
+struct drivers_t
+{
+  byte driver1;
+  byte driver2;
+  byte driver3;
+  byte driver4;
+  byte driver5;
+  byte driver6;
+};
+
+
+// Global variable to hold State of Health tlm packet.
+drivers_t drivers;
+
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+  information.id = 1;
+  
+  Serial.begin(57600);  // initialize serial port to transceiver and COSMOS
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  processCmds();
+  sendTlm();
+}
+
+void processCmds()
+{
+  while (Serial.available())
+  {
+    // get the new byte:
+    information.driver1 = Serial.read();
+    information.driver2 = Serial.read();
+    information.driver3 = Serial.read();
+    information.driver4 = Serial.read();
+    information.driver5 = Serial.read();
+    information.driver6 = Serial.read();
+    information.automatic = Serial.read();
+  }
+}
+
+void sendTlm()
+{
+  information.length = sizeof(information);
+  writeTlm((const char*)&information, sizeof(information));
+}
+
+void writeTlm(const char* pkt, byte size)
+{
+  for(int i=0; i<size; i++)
+  {
+    Serial.write(pkt[i]);
+  }
+}
