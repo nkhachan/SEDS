11ca15b038958b5225cd220199c41173e0b1b102
nkhachan <noopur.khachane@gmail.com>
2018-01-20 07:40:31 -0800

    add new stuff

--- Arduino Scripts/Arduino_in_Press_Table/Arduino_in_Press_Table.ino
+++ Arduino Scripts/Arduino_in_Press_Table/Arduino_in_Press_Table.ino
@@ -1,10 +1,10 @@
 /*
 Local Arduino in Bunker
  */
+int pressPin = 6;
+
 struct info_t
 {
-  byte length;
-  byte id;
   byte driver1;
   byte driver2;
   byte driver3;
@@ -15,29 +15,58 @@ struct info_t
   byte automatic;
 };
 
+struct press_t
+{
+  byte length;
+  byte id;
+  byte pressure;
+};
+
 // Global variable to hold State of Health tlm packet.
 info_t information;
-
+press_t valve_info;
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-  information.id = 1;
+  Serial.begin(57600);  // initialize serial port to transceiver and COSMOS
+  valve_info.id = 1;
   
   for(int i=0; i<6; i++)
   {
     pinMode(i, OUTPUT);
   }
-  
-  Serial.begin(57600);  // initialize serial port to transceiver and COSMOS
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-  processCmds();
-  setDrivers();
-  setServo();
+  //processCmds();
+  //setDrivers();
+  //setServo();
+  //readPressure();
+  sendTlm();
+  delay(1000);
 }
+
+void readPressure()
+{
+  valve_info.pressure = analogRead(pressPin);
+}
+
+void sendTlm() 
+ { 
+   valve_info.length = sizeof(valve_info); 
+   writeTlm((const char*)&valve_info, sizeof(valve_info)); 
+} 
+ 
+ 
+ void writeTlm(const char* pkt, byte size) 
+ { 
+   for(int i=0; i<size; i++) 
+   { 
+     Serial.write(pkt[i]); 
+   } 
+} 
 
 void processCmds()
 {
@@ -50,19 +79,19 @@ void processCmds()
     information.driver4 = Serial.read();
     information.driver5 = Serial.read();
     information.driver6 = Serial.read();
-    information.pot_reading = Serial.read();
+    //information.pot_reading = Serial.read();
     information.automatic = Serial.read();
   }
 }
 
 void setDrivers()
 {
-  digitalWrite(0, driver.driver1);
-  digitalWrite(1, driver.driver2);
-  digitalWrite(2, driver.driver3);
-  digitalWrite(3, driver.driver4);
-  digitalWrite(4, driver.driver5);
-  digitalWrite(5, driver.driver6);
+  digitalWrite(0, information.driver1);
+  digitalWrite(1, information.driver2);
+  digitalWrite(2, information.driver3);
+  digitalWrite(3, information.driver4);
+  digitalWrite(4, information.driver5);
+  digitalWrite(5, information.driver6);
 }
 
 void setServo()
@@ -78,3 +107,4 @@ void setServo()
     //Get shit from pot_reading
   }
 }
+
