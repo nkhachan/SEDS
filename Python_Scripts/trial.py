1b14dda99b915be2b440de80dce380d35cb4ae01
nkhachan <noopur.khachane@gmail.com>
2018-03-04 08:26:26 -0800

    added python scripts

---
+++ Python_Scripts/trial.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("")
+
+import os
+os.environ["COSMOS_USERPATH"] = "/home/noopur/Downloads/ballcosmos-0.0.1-py3-none-any.whl_FILES/ballcosmos"
+
+from ballcosmos.script import script
+print("HEllow")
