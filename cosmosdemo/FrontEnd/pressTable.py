e3616a9df2543b3766524b09f9a4b32e7c45921d
nkhachan <noopur.khachane@gmail.com>
2018-04-08 13:59:45 -0700

    remove old python code

--- cosmosdemo/FrontEnd/pressTable.py
+++
@@ -1,1 +0,0 @@
-from cosmos import *
--- cosmosdemo/FrontEnd/vulcan2.py
+++
@@ -1,7 +0,0 @@
-from cosmos import *
-
-def getThermoRead(int num):
-    return tlm('VULCAN2 THERMOCOUPLES THERMO' + str(num))
-
-def getPressRead(int num):
-    return tlm('VULCAN2 PRESS_TRANSDUCERS PRESS' + str(num))
